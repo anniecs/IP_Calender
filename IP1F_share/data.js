49680bbe14e7854831faf7772d3a62c68ad58873
anniecs <anniecs@users.noreply.github.com>
2026-09-11 16:40:28 +0800

    Refine shared calendar display

--- IP1F_share/data.js
+++ IP1F_share/data.js
@@ -34,7 +34,7 @@ window.IP1F_DATA = {
     },
     "2026-09-10": {
       "shared": "",
-      "grade1": "<div class=\"tag-header\"><span class=\"tag-pill pill-language-art\">Language Arts</span></div><ul><li>Review p. 27-32 and read out loud.</li><li>Vocabulary Quiz (p. 5, 7) tomorrow.</li></ul><div class=\"tag-header\" style=\"margin-top:4px;\"><span class=\"tag-pill pill-steam\">STEAM</span></div><ul><li>How Do Engineers Use Technology?</li></ul><div class=\"tag-header\"><span class=\"tag-pill pill-brother\">弟弟聯絡本</span></div><ul><li>複習「國語習作」第一課、第二課</li></ul>"
+      "grade1": "<div class=\"tag-header\"><span class=\"tag-pill pill-language-art\">Language Arts</span></div><ul><li>Review p. 27-32 and read out loud.</li></ul><div class=\"tag-header\" style=\"margin-top:4px;\"><span class=\"tag-pill pill-steam\">STEAM</span></div><ul><li>How Do Engineers Use Technology?</li></ul><div class=\"tag-header\"><span class=\"tag-pill pill-brother\">弟弟聯絡本</span></div><ul><li>複習「國語習作」第一課、第二課</li></ul>"
     },
     "2026-09-11": {
       "shared": "",
